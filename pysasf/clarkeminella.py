Remove dead code and fix randon_props_subsamples masking
Dead code cleanup (no public API change):
- delete pysasf/statimodels_OLS.py (untracked stray statsmodels test script)
- plots.py: drop broken duplicate props_histo(bd) and incomplete boxplot()
- clarkeminella.py: drop commented-out blocks, unused matplotlib import,
  and unused CVs/areas_medias accumulators
- solvers.py: drop commented solve_ols_sm and unused statsmodels import
- basindata.py: drop commented-out lines

Fix stats.randon_props_subsamples:
- combs[idxs and feas] raised ValueError (bool of array); rebuilt as
  composable boolean masks combined with &
- the target filter is now actually applied (was computed and discarded)

Co-Authored-By: Claude Sonnet 5 <noreply@anthropic.com>

pysasf/clarkeminella.py
@@ -8,7 +8,6 @@ Created on jul 2024
 """
 
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 from IPython.display import clear_output
 from scipy.spatial import ConvexHull
@@ -20,26 +19,10 @@ import time
 from pysasf import solvers
 from pysasf import stats
 
-
-#def confidence_region(P, p = 95, space_dist='mahalanobis0'):
-#    d = space_dist
-#    return stats.confidence_region(P, p = 95, space_dist=d)
-    
-#def cm_feasebles(Ps):
-#    Ps_feas = Ps[[np.all(P>0) for P in Ps]]
-#    return np.array(Ps_feas)
-
-#########################################################################################
-    
-
-
-
 def run_repetitions_and_reduction (bd, key, reductions, percents = False,
                                    repetitions = 50, target = None):
     inicio = time.time()
     cv = lambda x: np.std(x) / np.mean(x) *100
-    CVs = []
-    areas_medias = []
     filename = bd.filename+'_'+str(key)
     
     # create the columns names for return dataframe
@@ -69,52 +52,36 @@ def run_repetitions_and_reduction (bd, key, reductions, percents = False,
         hull = ConvexHull(pts)
         area = hull.volume # area for 2d points
         return area
-    
-    
-    #AQUI ACONTECEM AS REDUÇÕES
+
+    # loop over the sample-size reductions
     for n in reductions:
-        #Total=prod*n
         points_set = []
         areas = []
         filename = filename+'-'+str(n)
 
         t1 = time.time()
         for i in range(repetitions):
-            # apenas para imprimir o texto
             t2 = time.time()
             if t2-t1>0.5:
-                #clear_output(wait=True)
-                print ('Processing for', n, 'subsamples of',key, 
+                print ('Processing for', n, 'subsamples of',key,
                    ', repetition number', i+1,end='\r', flush=True)
                 t1=t2
 
-            
-            #_,Ptot = stats.randon_props_subsamples(bd, key, n, only_feasebles=False)
-            #Pfea =  cm.cm_feasebles(Ptot[:,0:2])
-            _,Pfea = stats.randon_props_subsamples(bd, key, n, 
+            _,Pfea = stats.randon_props_subsamples(bd, key, n,
                                                    only_feasebles=True, target=target)
 
-            
             if Pfea.shape[0]>=4:
                 Pcr = stats.confidence_region(Pfea[:,0:2], p = 95)
-                #<<-------------------------------
-                #hull = ConvexHull(Pcr[:,0:2])    #Aqui
-                #areas.append(hull.volume)
-                #>>-------------------------------
                 points_set.append(Pcr)
-                #-------------------------------
-       
-        #Aqui: otimização/paralelização
-        #>>-------------------------------
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-             areas = list(executor.map(compute_area, points_set)) 
-        #-------------------------------
 
-        
+        # areas of the confidence regions, computed in parallel
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+             areas = list(executor.map(compute_area, points_set))
+
+
         # insert data for df_out
         if percents == True:
             idx = np.argwhere(reductions == n)[0][0]
-            #print('IDX',idx)
             nSamp = percents_reductions[idx]
         else:
             nSamp = n
@@ -130,9 +97,6 @@ def run_repetitions_and_reduction (bd, key, reductions, percents = False,
         df_out_data.append(df_out_data_n)
         bd.cm_df = df_out_data
 
-        CVs.append(cv(areas))
-        areas_medias.append(np.mean(areas))
-
     # Clean the terminal and print some infos
     print('Done!')
     clear_output(wait=True)
@@ -147,85 +111,3 @@ def run_repetitions_and_reduction (bd, key, reductions, percents = False,
     print ('Saving in', filename+'.csv')
     df_out.to_csv(bd.output_folder+'/'+filename+'.csv')
     return (df_out)
-###########################################################################################3
-
-
-
-#'''
-#Esta função processa o cálculo das porcentagens usando as médias
-#Faz as contas usando reps repetições tomando aletoriamente um conjunto
-#de subamostras com tamanhos definidos por n_list. Tem opção de plotar. 
-#'''
-
-#def random_subsamples(bd,nlist):
-#    df_dict = bd.df_dict
-#    ss_dict = {}
-#    for i, key in enumerate(df_dict.keys()):
-#        data = df_dict[key].values.astype(float)
-#        ss_dict[key]=data[np.random.choice(len(data), nlist[i], replace=False)]
-#    return ss_dict
-    
-#'''
-#'''
-#def get_props_from_subsamples_means(bd,reps,n_list, plot=True):
-#    df_dict = bd.df_dict
-#    Y = bd.df_dict['Y'].values.astype(float)
-#    S_inv = np.linalg.inv(np.cov(Y.T))
-#    
-#    if len(df_dict.keys())!=len(n_list):
-#        print("The n_list needs to be of the same size of lands data.")
-#    else:
-#        Ps = []
-#        for i in range(reps):
-#            ss_dict = random_subsamples(bd,n_list)
-#            X = []
-#            for key in bd.sources:
-#                X.append(ss_dict[key].mean(axis=0))
-#            X = np.array(X).T
-#            y = ss_dict['Y'].mean(axis=0)
-#            P = solvers.solve_gls_4x4(y,X,S_inv)
-#            #print(P)
-#            if np.all(np.array(P[0:3])>=0):
-#                #print (P[0:3], 'soma:', np.sum(P[0:3]))    
-#                Ps.append(list(P))
-#        Ps = np.array(Ps)
-#        if plot:
-#            plt.plot(Ps[:,0],Ps[:,1],'.')
-#            plt.xlabel("P1")
-#            plt.ylabel("P2")
-#            plt.xlim((0,1))
-#            plt.ylim((0,1))
-#            plt.plot(Ps[:,0],Ps[:,1],'.')
-#            plt.show()
-#        return(Ps)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

pysasf/plots.py
@@ -60,11 +60,8 @@ def draw_hull(P, x_col=0, y_col=1, savefig = False,
     plt.figure(figsize=(4, 4))  
 
     points = np.vstack((P[:,x_col],P[:,y_col])).T
-    #print(points.shape)
     hull = ConvexHull(points)
-    
-    #for simplex in hull.simplices:
-        #plt.plot(points[simplex, 0], points[simplex, 1], 'k-',lw=1)
+
     def _plot():
         for simplex in hull.simplices:
             plt.plot(points[simplex, 0], points[simplex, 1], 'k-',lw=1)
@@ -94,61 +91,15 @@ def props_histo(props):
     n = props.shape[0]
     dim = props.shape[1]
     plt.figure(figsize=(5*dim,3))
-    
-    for j in range(dim):
-        plt.subplot(1,dim,j+1)
-        n, bins, patches = plt.hist(props[:,j], bins=50) 
-        plt.title('P'+str(j+1))
-    plt.show()
 
-def props_histo(bd):
-    
-    bd.df_dict
-    n = props.shape[0]
-    dim = props.shape[1]
-    plt.figure(figsize=(5*dim,3))
-    
     for j in range(dim):
         plt.subplot(1,dim,j+1)
-        n, bins, patches = plt.hist(props[:,j], bins=50) 
+        n, bins, patches = plt.hist(props[:,j], bins=50)
         plt.title('P'+str(j+1))
     plt.show()
 
 
 def data_histo(bd):
     for s in bd.sources:
-        #print(s)
         df = bd.df_dict[s]
         df.hist()
-
-'''
-Data is a nSources x nReducts x nProps
-Sources is a string list of sources key for labels
-Reductions e a list of numbers of samples or percentages for reductions
- '''
-def boxplot(data, sources, reductions, in_percents = True):
-    
-# Dados de exemplo
-#dados = [np.random.normal(50, 10, 200), np.random.normal(60, 15, 200)]
-
-# Criando o boxplot com personalização
-    box = plt.boxplot(dados, 
-                  vert=True, 
-                  patch_artist=True, 
-                  notch=True, 
-                  showmeans=True, 
-                  labels=['Conjunto 1', 'Conjunto 2'])
-
-# Personalizando as cores
-#cores = ['lightblue', 'lightgreen']
-#for patch, cor in zip(box['boxes'], cores):
-#    patch.set_facecolor(cor)
-
-# Adicionando rótulos
-#plt.title('Boxplot Personalizado')
-#plt.ylabel('Valores')
-
-# Exibindo o gráfico
-#plt.show()
-
-

pysasf/stats.py
@@ -50,31 +50,27 @@ def std(bd):
 
 def randon_props_subsamples(bd, key, n, only_feasebles=False, target=None):
 
-    key_idx = list(bd.df_dict.keys()).index(key) # indice da fonte
+    key_idx = list(bd.df_dict.keys()).index(key) # coluna da fonte em combs
     size = len(bd.df_dict[key]) # tamanho da amostra daquela fonte
-    rand = np.random.choice(np.arange(size), n, replace=False) # sorteia n 
-    feas = bd.feas
-    Ps = bd.props #pega as proporções já calculadas
-    combs = bd.combs
-    
-    # if 'bytarget' pick only the target selected
-    targets_idx = list(bd.df_dict.keys()).index('Y')
-    if target != None:
-        idxs = np.where(np.isin(combs[:,targets_idx],target))[0]#seleciona por target
+    rand = np.random.choice(np.arange(size), n, replace=False) # sorteia n indices
 
-  
-    # if only_feasebles is true pick only feaselbes soluctions
-    if only_feasebles == True:
-        #print('randon_props_subsamples->only_feasebles')
-        idxs = np.where(np.isin(combs[:,key_idx],rand))[0]
-        selected_combs = combs[idxs and feas]
-        selected_Ps = Ps[idxs and feas]
+    Ps = bd.props                    # proporções já calculadas
+    combs = bd.combs                 # combinações de indices de amostras
+    feas = np.asarray(bd.feas, dtype=bool)  # máscara booleana das soluções factíveis
 
-    else:
-        selected_combs = combs[np.where(np.isin(combs[:,key_idx],rand))]
-        selected_Ps = Ps[np.where(np.isin(combs[:,key_idx],rand))]
-        
-    return selected_combs, selected_Ps
+    # linhas cujo indice de amostra da fonte 'key' está no subconjunto sorteado
+    mask = np.isin(combs[:, key_idx], rand)
+
+    # se 'target' for informado, restringe às combinações daquele(s) alvo(s) em Y
+    if target is not None:
+        targets_idx = list(bd.df_dict.keys()).index('Y')
+        mask &= np.isin(combs[:, targets_idx], target)
+
+    # se only_feasebles, mantém apenas as soluções factíveis
+    if only_feasebles:
+        mask &= feas
+
+    return combs[mask], Ps[mask]
 
 
 